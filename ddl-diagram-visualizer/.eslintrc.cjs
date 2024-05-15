ff54296c1e5cbc4f051eb542b583c3efe9c01bc5
kappakun17 <86644372+kappakun17@users.noreply.github.com>
2024-05-15 23:42:03 +0900

    eslint設定

--- ddl-diagram-visualizer/.eslintrc.cjs
+++ ddl-diagram-visualizer/.eslintrc.cjs
@@ -2,16 +2,15 @@ module.exports = {
   root: true,
   env: { browser: true, es2020: true },
   extends: [
-    "eslint:recommended",
-    "plugin:@typescript-eslint/recommended",
-    "plugin:react/recommended",
-    "plugin:react/jsx-runtime",
-    "plugin:react-hooks/recommended",
-    "prettier",
+    'eslint:recommended',
+    'plugin:@typescript-eslint/recommended',
+    'plugin:react/recommended',
+    'plugin:react/jsx-runtime',
+    'plugin:react-hooks/recommended',
   ],
-  ignorePatterns: ["dist", ".eslintrc.cjs", "vite.config.js"],
-  parser: "@typescript-eslint/parser",
-  plugins: ["react-refresh"],
+  ignorePatterns: ["dist", ".eslintrc.cjs", "vite.config.js"], 
+  parser: '@typescript-eslint/parser',
+  plugins: ['react-refresh'],
   rules: {
     "react-refresh/only-export-components": [
       "warn",
@@ -20,13 +19,7 @@ module.exports = {
     "@typescript-eslint/naming-convention": [
       "error",
       {
-        selector: [
-          "variable",
-          "parameter",
-          "method",
-          "property",
-          "typeParameter",
-        ],
+        selector: ["variable", "parameter", "method", "property", "typeParameter"],
         format: ["camelCase"],
       },
       {
@@ -41,6 +34,6 @@ module.exports = {
         selector: ["enumMember"],
         format: ["UPPER_CASE"],
       },
-    ],
+    ]
   },
 };
